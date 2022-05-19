5cfc4200e9499852e8c6baeb3d4156c76b3cabc9
Miroslava <miroslava.jindrova@zi-mannheim.de>
2022-05-19 14:55:48 +0200

    started to evaluate ders questionnaire, not finished yet

--- self_reports.R
+++ self_reports.R
@@ -2,6 +2,7 @@
 #Miroslava Jindrova, ZI Mannheim, 2021
 
 library(dplyr)
+library(readxl)
 
 #read the data
 pre<-read.csv('//hobbes/daten/PSM/Brainboost/evasys/BBpre_raw.csv', sep = ';')
@@ -25,4 +26,20 @@ pre<-mutate(pre, timepoint=rep('pre',length(subject)))
 post<-mutate(post, timepoint=rep('post',length(subject)))
 fu<-mutate(fu, timepoint=rep('fu',length(subject)))
 
-ders<-rbind(select(pre,c(2,'timepoint',57:92)),select(post,c(2,'timepoint',57:92)),select(fu,c(2,'timepoint',92:127)))
+#add group info and remove drop outs
+#add info about randomization and drop the participants that dropped out before post 
+demographics<-read_xlsx('Y:/Projects/BrainBoost/data_analysis/demographics.xlsx')
+group_info<-select(filter(demographics, is.na(Comments)|!Comments=='dropped out'), subject=Subject, group=Group)
+group_info<-mutate(group_info, subject=sub('sub','sub-',subject))
+pre<-merge(pre, group_info, by='subject')
+post<-merge(post, group_info, by='subject')
+fu<-merge(fu, group_info, by='subject')
+
+#extract ders questionnaire
+ders<-rbind(select(pre,c('subject','timepoint','group',57:92)),select(post,c('subject','timepoint','group',57:92)),select(fu,c('subject','timepoint','group',92:127)))
+#calculate the score (items 1,3,6,7,8,10,17,20,22,24 and 34 are reverse score items)
+
+colnames(ders)[4:39]<-c(paste(rep('i',36),as.character(c(1:36)),collapse = '')) #this stil doesn't work, I want to have i1 to i36
+ders<-mutate(ders, total=-'1'+'2')
+
+ders<-mutate(ders, total=-.[[4]]+.[[5]]-.[[6]]+.[[7]]+.[[8]]-.[[9]]-.[[10]]-.[[11]]+.[[12]]-.[[13]]+.[[14]]+.[[15]]+.[[16]]+.[[17]]+.[[18]]+.[[19]]-.[[20]]+.[[21]]+.[[22]]-.[[23]]+.[[24]]-.[[25]]+.[[26]]-.[[27]]+.[[28]]+.[[29]]+.[[30]]+.[[31]]+.[[32]]+.[[33]]+.[[34]]+.[[35]]+.[[36]]-.[[37]]+.[[38]]+.[[39]])
