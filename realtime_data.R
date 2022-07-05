8e56ec2bee01cf84d4edbefb3000db3cb774bac0
Miroslava <miroslava.jindrova@zi-mannheim.de>
2022-07-05 16:24:20 +0200

    adjusted paths, edited plots

--- realtime_data.R
+++ realtime_data.R
@@ -13,7 +13,7 @@ library(effectsize)
 
 #load data
 setwd('//zi.local/flstorage/dep_psm/group_psm/AG-Paret/Projects/BrainBoost/data_analysis/realtime')
-data<-read.csv('trainingData20220330T181545.csv')
+data<-read.csv('trainingData20220705T124626.csv')
 
 #extract relevant data
 data$functional_image_fn[which(data$subject=='sub-14'& data$session=='ses-training3')]<-str_replace(data$functional_image_fn[which(data$subject=='sub-14'& data$session=='ses-training3')],'SUB14_NF3.SUB14_NF3','SUB14NF3.SUB14NF3') #correct naming for sub-14, ses-training3
@@ -38,7 +38,7 @@ PES<-merge(PES,summarise(roiData, pooledSD=sd_pooled(z_score[which(condition=='V
 PES<- mutate(PES, blockES=(DOWN_trial-VIEW_trial)/pooledSD)
 
 #add info about dropping out and remove these subjects
-demographics<-read_xlsx('//zi.local/flstorage/dep_psm/group_psm/AG-Paret/Projects/BrainBoost/data_analysis/demographics.xlsx')
+demographics<-read_xlsx('Y:/Projects/BrainBoost/data_analysis/demographics.xlsx')
 group_info<-select(filter(demographics, is.na(Comments)|!Comments=='dropped out'), subject=Subject)
 group_info<-mutate(group_info, subject=paste('sub-',substr(subject,4,5),sep=""))
 PES<-merge(PES, group_info, by='subject')
@@ -50,7 +50,7 @@ PES<-group_by(PES, subject, session, run)
 cycP<-summarise(PES, PES=mean(blockES))
 
 #plots
-ggplot(cycP,aes(run,PES))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_normal,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))
+ggplot(cycP,aes(run,PES))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_normal,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))+ggtitle('Personal effect size')
 
 ggplot(cycP,aes(session,PES))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_normal,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))
 
@@ -79,7 +79,7 @@ aggrData<-mutate(aggrData, session=as.numeric(substr(session,13,13)), NFrun=as.n
 aggrData<-mutate(aggrData, run=(session-1+(NFrun-1+session)))
 
 #plots
-ggplot(aggrData,aes(run,raw_fb))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_boot,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))
+ggplot(aggrData,aes(run,raw_fb))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_boot,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))+ggtitle('Raw feedback value during training')
 
 ggplot(aggrData,aes(session,raw_fb))+stat_summary(fun.y = mean,geom = 'point')+stat_summary(fun.y = mean,geom = 'line', aes(group=1))+stat_summary(fun.data = mean_cl_boot,geom = 'errorbar', width=0.4)+scale_x_continuous(breaks = c(1:6))
 
